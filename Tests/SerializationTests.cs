94cf1b335a4bee98e4be45f007a2f927db9003ca
Alexander Travkin <aleverdes.alex@gmail.com>
2023-01-29 13:36:43 +0400

    Updated tests

--- Tests/SerializationTests.cs
+++ Tests/SerializationTests.cs
@@ -14,9 +14,15 @@ public class SerializationTests
         var testSerializableMessage = new TestSerializableMessage()
         {
             IntValue = 123,
+            FloatValue = 21.05f,
             StringValue = "Test",
             Vector3Value = Vector3.forward,
-            EnumValue = TestEnum.Second
+            EnumValue = TestEnum.Second,
+            BoolValue = true,
+            FloatArray = new []{1f, 2f, 3f},
+            IntArray = new []{ 2, 4, 6 },
+            UshortArray = new ushort[] { 3, 5, 7 },
+            ColorValue = Color.red
         };
 
         var sw = Stopwatch.StartNew();
@@ -30,23 +36,14 @@ public class SerializationTests
         Debug.Log("Deserialization: " + sw.Elapsed);
         
         Assert.AreEqual(123, result.IntValue);
+        Assert.AreEqual(21.05f, result.FloatValue);
         Assert.AreEqual("Test", result.StringValue);
         Assert.AreEqual(Vector3.forward, result.Vector3Value);
         Assert.AreEqual(TestEnum.Second, result.EnumValue);
-    }
-    
-    private class TestSerializableMessage : SerializableMessage
-    {
-        public int IntValue;
-        public string StringValue;
-        public Vector3 Vector3Value;
-        public TestEnum EnumValue;
-    }
-
-    private enum TestEnum
-    {
-        First,
-        Second,
-        Third
+        Assert.AreEqual(true, result.BoolValue);
+        Assert.AreEqual(2f, result.FloatArray[1]);
+        Assert.AreEqual(4, result.IntArray[1]);
+        Assert.AreEqual(5, result.UshortArray[1]);
+        Assert.AreEqual(1f, result.ColorValue.r);
     }
 }
